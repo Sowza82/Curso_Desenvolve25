39b80f987670a2404d51f466df904a1ad8dbec0e
Tatiane Souza <tathyannesg18@gmail.com>
2025-07-09 17:29:24 -0300

    Iniciando módulo 02

---
+++ módulo 02/javascript/aula01node/koru/aula01.js
@@ -0,0 +1,6 @@
+console.log("Olá, mundo!");
+console.log("Sou Tatiane Souza");
+console.log("Tenho 43 anos");
+console.log("Dev Front Júnior");
+console.log("Estou iniciando JavaScript no Desenvolve");
+console.log("Vamos lá!");
